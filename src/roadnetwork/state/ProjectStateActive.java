96cec75deb7fceb77b768d365e5cc170b0ffa432
Helder Faria 1140816 <>
2015-12-17 12:51:59 +0000

    Introdução dos estados do Project

---
+++ src/roadnetwork/state/ProjectStateActive.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roadnetwork.state;
+
+/**
+ *
+ * @author André Pedrosa, Hélder Faria, José Miranda, Rubén Rosário
+ */
+public class ProjectStateActive extends ProjectStateImpl{
+    
+}
