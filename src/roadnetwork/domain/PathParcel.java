a8a668820eff86e860d1de931ebbf1af1b6063c7
Helder Faria <1140816@isep.ipp.pt>
2016-01-06 22:58:27 +0000

    Nova classe PathParcel e Enum SimDirection

---
+++ src/roadnetwork/domain/PathParcel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roadnetwork.domain;
+
+/**
+ *
+ * @author André Pedrosa, Hélder Faria, José Miranda, Rubén Rosário
+ */
+public class PathParcel {
+    
+    Section m_section;
+    Segment m_segment;
+    SimDirection m_direction;
+    Double m_exitTime;
+
+    
+    
+    
+ 
+}
